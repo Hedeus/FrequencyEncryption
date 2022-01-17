19c26e5fbd27c2c1992f8bcb3c0da5521f469a65
Hedeus <hedeus1987@gmail.com>
2022-01-17 13:54:16 +0200

    Построена базовая структура MVVM проекта

---
+++ FrequencyEncryption/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,12 @@
+﻿using FrequencyEncryption.Infrastructure.Commands.Base;
+using FrequencyEncryption.ViewModels.Base;
+
+namespace FrequencyEncryption.ViewModels
+{
+    internal class MainWindowViewModel : ViewModel
+    {
+        
+
+
+    }
+}
